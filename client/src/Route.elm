ed55e49f428271736357995dc1cd2e3407c42f5c
David Burkett <david@davidburkett.xyz>
2021-03-01 18:40:27 -0500

    Added a Route module

---
+++ client/src/Route.elm
@@ -0,0 +1,40 @@
+module Route exposing (Route(..), parse, push, replace)
+
+import Browser.Navigation as Navigation
+import GameId exposing (GameId)
+import Url exposing (Url)
+import Url.Parser as Parser exposing (Parser)
+
+
+type Route
+    = Home
+    | Game GameId
+
+
+parse : Url -> Maybe Route
+parse =
+    Parser.parse <|
+        Parser.oneOf
+            [ Parser.map Home Parser.top
+            , Parser.map (Game << GameId.fromString) Parser.string
+            ]
+
+
+toString : Route -> String
+toString route =
+    case route of
+        Home ->
+            "/"
+
+        Game gameId ->
+            "/" ++ GameId.toString gameId
+
+
+push : Navigation.Key -> Route -> Cmd msg
+push navigationKey =
+    Navigation.pushUrl navigationKey << toString
+
+
+replace : Navigation.Key -> Route -> Cmd msg
+replace navigationKey =
+    Navigation.replaceUrl navigationKey << toString
